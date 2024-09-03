697371daf66217867bd64b38a7ca9d68931a1020
Mércia Regina da Silva <39158160+MerciaReginasl@users.noreply.github.com>
2024-09-03 10:40:56 -0300

    Update and rename semaforo.hpp to diagramadeclasses_main.cpp

--- diagramadeclasses_main.cpp
+++ diagramadeclasses_main.cpp
@@ -24,3 +24,41 @@
                   |                  /
                   |                 /
                   +-----------------+
+
+
+                  
+Main
+directionalAssociation Pessoa
+association ContaBancaria
+---
+- pessoas: Pessoa[]
+---
++ main(): int
+---
+
+ContaBancaria
+---
+- static PROX_NUM_CONTA: int
+- static TOTAL_DE_CONTAS: int
+- proprietario: Pessoa
+- numero: int
+- saldo: float
+---
++ ContaBancaria(proprietario: Pessoa)
++ ~ContaBancaria(): void
++ static visualizarTotalDeContas(): int
++ sacar(valor: float): void
++ depositar(valor: float): void
++ visualizar(): void
++ getProprietario(): Pessoa
++ getNumero(): int
++ getSaldo(): float
+---
+
+Pessoa
+composes ContaBancaria
+---
+- contasBancaria: ContaBancaria
+---
++ adicionarContaBancaria(): void
+---
